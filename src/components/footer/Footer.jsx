a65cf296c46b62deae623de33fee697545fa0ee7
shubham <shubhamrupapara993@gmail.com>
2023-07-14 11:23:21 +0530

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
-  )
-}
+    <section className="f-wrapper">
+      <div className="paddings innerWidth flexCenter f-container">
+        <div className="flexColStart f-left">
+          <img src="./logo2.png" alt="" width={120} />
 
-export default Footer
+          <span className="secondaryText">
+            Our vision is to make all people <br />
+            the best place to live for them.
+          </span>
+        </div>
+
+        <div className="flexColStart f-right">
+          <span className="primaryText">Information</span>
+          <span className="secondaryText">145 Ahmedabad, FL 4571, India</span>
+
+          <div className="flexCenter f-menu">
+            <span>Property</span>
+            <span>Services</span>
+            <span>Product</span>
+            <span>About Us</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;

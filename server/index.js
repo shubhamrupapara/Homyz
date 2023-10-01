c0e8f77134d371fd7d4ee308d16e686ee9dbee30
shubham <shubhamrupapara993@gmail.com>
2023-10-01 13:30:22 +0530

    Routing

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { userRoute } from "./routes/userRoute.js";
+import { residencyRoute } from "./routes/residencyRoute.js";
 dotenv.config();
 
 const app = express();
@@ -13,3 +15,6 @@ app.use(cors());
 app.use(cookieParser());
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.use('/api/user', userRoute)
+app.use("/api/residency", residencyRoute)
